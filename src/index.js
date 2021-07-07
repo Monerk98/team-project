479ce4d9da7a8d62179894a723fc4db0472d7a67
Ivan <lorf1991@gmail.com>
2021-07-07 20:23:26 +0300

    script

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import './sass/main.scss';
+
+// плавный скролл по якорям
+function runSmothLinks() {
+  const smoothLinks = document.querySelectorAll('a[href^="#"]');
+
+  for (let smoothLink of smoothLinks) {
+    smoothLink.addEventListener('click', function (e) {
+      e.preventDefault();
+      const id = smoothLink.getAttribute('href');
+
+      document.querySelector(id).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    });
+  }
+}
+
+runSmothLinks();
