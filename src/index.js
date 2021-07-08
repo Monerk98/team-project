c6fd01f996dfcde6690629720e77b8d19039fdbe
Ivan <lorf1991@gmail.com>
2021-07-08 11:09:12 +0300

    base script Up

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './sass/main.scss';
 
-// плавный скролл по якорям
+//========= плавный скролл по якорям =======
 function runSmothLinks() {
   const smoothLinks = document.querySelectorAll('a[href^="#"]');
 
@@ -16,5 +16,80 @@ function runSmothLinks() {
     });
   }
 }
+//==========END плавный скролл по якорям=========
+
+//=======кнопка Up adn Down===========
+var $heightResize = $(window).height();
+$(window).resize(function () {
+  $heightResize = $(window).height();
+});
+
+var flagF = true;
+var flagClick = true;
+var scrolling = 0;
+var $selector = $('html, body'),
+  $buttonUpDown = $('button.up'),
+  /* Определить, вверх или вниз*/
+  // Текущее значение скролла
+  $scrollCurent = 0,
+  // Предыдущее значение
+  $scrollTemp = 0;
+
+//Скролл окна
+$(window).scroll(function () {
+  $scrollCurent = $selector.scrollTop();
+  // Появление кнопки
+  if (flagClick) {
+    $buttonUpDown.css({
+      right: '20px',
+    });
+  } else if ($scrollCurent > $heightResize / 3) {
+    $buttonUpDown.css({ right: '20px' });
+  } else {
+    $buttonUpDown.css({ right: '-100px' });
+  }
+  // прокрутка скролла вверх
+  if ($scrollTemp > $scrollCurent) {
+  } else {
+    //Если крутим вниз то збиваем значения на дефолтные
+    flagClick = false;
+    scrolling = 0;
+    flagF = true;
+    $buttonUpDown.text('Up');
+  }
+  // присваиваем значения, одно всегда будет больше
+  $scrollTemp = $scrollCurent;
+});
+//
+
+//Клик по кнопке верх-вниз
+$buttonUpDown.on('click', function (e) {
+  if (flagF) {
+    scrolling = $selector.scrollTop();
+    $selector.animate(
+      {
+        scrollTop: 0,
+      },
+      500,
+    );
+    flagF = false;
+    flagClick = true;
+    flagClick = true;
+    e.target.textContent = 'Down';
+    // console.log("Клик по кнопке - вверх " + scrolling)
+  } else {
+    $selector.animate(
+      {
+        scrollTop: scrolling,
+      },
+      500,
+    );
+    flagF = true;
+    flagClick = true;
+    e.target.textContent = 'Up';
+    //console.log("Клик по кнопке - вниз "+scrolling);
+  }
+});
+//======= END кнопка Up adn Down===========
 
 runSmothLinks();
