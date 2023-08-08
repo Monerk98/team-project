bdc8d27e47b9fd603dfa4d739b5ed46c59f1c837
Monerk98 <boldv98@gmail.com>
2023-08-08 07:12:59 +0300

    reb

--- src/js/menu.js
+++ src/js/menu.js
@@ -2,7 +2,8 @@
     const menuBtnRef = document.querySelector("[data-menu-button]");
     const mobileMenuRef = document.querySelector("[data-menu]");
     const mobileBtnClose = document.querySelector("[data-menu-close]");
-  
+    
+  if(!menuBtnRef || !mobileMenuRef || !mobileBtnClose) return 
     menuBtnRef.addEventListener("click", () => {
       mobileMenuRef.classList.toggle("is-open");
     });
